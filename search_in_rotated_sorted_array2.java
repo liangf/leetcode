ebf91a376714046ac2fd237683614531a1cd6240
Liang Fang <mailfangliang@gmail.com>
2017-05-16 22:31:06 -0400

    search in array2

---
+++ search_in_rotated_sorted_array2.java
@@ -0,0 +1,19 @@
+
+
+
+public class search_in_rotated_sorted_array2 {
+	
+    public boolean search(int[] nums, int target) {
+    
+    	for (int i=0; i<nums.length; ++i) {
+    		if (nums[i] == target) {
+    			return true;
+    		}
+    	}    
+    	return false;
+    }
+
+	public static void main(String[] args) {
+
+	}
+} 
