63e663e25e795f22d2dff20f76f3d4a17922b5c2
Liang Fang <mailfangliang@gmail.com>
2017-05-17 20:22:04 -0400

    valid bst method2

--- validate_binary_search_tree.java
+++ validate_binary_search_tree.java
@@ -1,5 +1,5 @@
 
-
+import java.util.*;
 
 public class validate_binary_search_tree {
 	class TreeNode {
@@ -8,7 +8,7 @@ public class validate_binary_search_tree {
 		TreeNode right;
 		TreeNode(int x) { val = x;}
 	}
-
+/*
     public boolean isValidBST(TreeNode root) {
         return help(root, null, null);
     }	
@@ -29,7 +29,27 @@ public class validate_binary_search_tree {
     	boolean right = help(root.right, root, max);
     	return left && right;
     }	
+*/
 
+    public boolean isValidBST(TreeNode root) {
+    	List<Integer> pre = new ArrayList<Integer>();
+        return help(root, pre);
+    }	
+
+    public boolean help(TreeNode root, List<Integer> pre) {
+    	if (root == null) return true;
+
+    	boolean left = help(root.left, pre);
+    	if (pre.size() > 0) {
+    		if (root.val <= pre.get(0)) return false;
+    		pre.set(0, root.val);
+    	} else {
+    		pre.add(root.val);
+    	}
+    	boolean right = help(root.right, pre);
+    	return left && right;
+    }    
+    
     public static void main(String[] args) {
 
     }
