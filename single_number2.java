b6db7d24fcd3285e8e19b23f593ddf94dbd28760
Liang Fang <mailfangliang@gmail.com>
2017-05-27 18:36:40 -0400

    single number

---
+++ single_number2.java
@@ -0,0 +1,20 @@
+
+
+public class single_number2 {
+	
+    public int singleNumber(int[] nums) {
+    	int res = 0;
+    	for (int i=0; i<32; ++i) {
+    		int count = 0;
+    		for (int j=0; j<nums.length; ++j) {
+    			count += (nums[j] >> i) & 1;
+    		}
+    		res += (count % 3) << i;
+    	}
+    	return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
